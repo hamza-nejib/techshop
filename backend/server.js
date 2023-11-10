b82d688638e38b8ebac23e284148cd773a0d3657
hamza-nejib <hamza-nejib@outlook.com>
2023-11-10 15:45:27 +0100

    build4

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,14 @@ connectDB();
 
 const app = express();
 
-app.use('/api/products', productRoutes);
+//app.use('/api/products', productRoutes);
 
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-    app.get('*', (req, res) =>
+    app.get('/', (req, res) =>
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     );
 } else {
