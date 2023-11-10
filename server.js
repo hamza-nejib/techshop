4fbc7ee2d654ff6a3260bd956699be49a5be54de
hamza-nejib <hamza-nejib@outlook.com>
2023-11-10 15:36:58 +0100

    build3

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use('/api/products', productRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
